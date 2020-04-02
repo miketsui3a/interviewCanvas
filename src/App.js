81e9a66cf69228ea651b2fbff7471ea86e3de12c
Mike Tsui <miketsui3a@yahoo.com.hk>
2020-04-02 22:25:09 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CanvasComponent from './CanvasComponent.js';
 import TestCanvas from './TestCanvas.js';
 
 function App() {
   return (
     <div className="App">
-      {/* <CanvasComponent /> */}
       <TestCanvas />
     </div>
   );
